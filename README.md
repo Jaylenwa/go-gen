997f2289496f54dbea38adaa2946eee3f345479b
Jaylenwa <1318248167@qq.com>
2024-05-11 17:46:07 +0800

    添加命令行执行

--- README.md
+++ README.md
@@ -3,9 +3,11 @@
 
 ## 使用方法
 
-前置条件
+**前置条件说明**
 
-配置 `.gen-cli.yaml`
+方法1，方法2；需要配置`.gen-cli.yaml`方可使用
+
+方法3（命令行模式）无需配置
 
 配置内容见文件注释
 
@@ -23,3 +25,14 @@ go build .
 # linux下执行
 ./gogen --config gen .gen-cli.yaml
 ```
+
+### 3、使用命令行（推荐）
+**说明**：[可执行文件名]为 go build 后的可执行文件
+
+使用 `[可执行文件名] gen --help` 查看命令行参数帮助列表
+
+使用案例：
+```shell
+[可执行文件名] gen -a 127.0.0.1 -u root --pwd root -p 3306 -d dbName -t tableName -s serverName
+```
+还有额外参数可以配置，使用`[可执行文件名] gen --help`命令查看 按需填写
--- cmd/gen.go
+++ cmd/gen.go
@@ -10,7 +10,7 @@ import (
 )
 
 var (
-	addr, user, pwd, port, db, table, serverName string
+	addr, user, pwd, port, db, table, serverName, goModule, goVersion, codePath string
 )
 
 // genCmd represents the gen command
@@ -19,6 +19,10 @@ var genCmd = &cobra.Command{
 	Short: "automatically generate go files for ORM model, service, repository, handler",
 	Long:  `automatically generate go files for ORM model, service, repository, handler`,
 	Run: func(cmd *cobra.Command, args []string) {
+		println(1)
+		i := viper.Get("server.go_version")
+		println(i.(string))
+		println(2)
 		addr = viper.Get("mysql.addr").(string)
 		user = viper.Get("mysql.user").(string)
 		pwd = viper.Get("mysql.pwd").(string)
@@ -42,7 +46,24 @@ func init() {
 	genCmd.Flags().StringVarP(&port, "port", "p", "3306", "Enter MySQL port")
 	genCmd.Flags().StringVarP(&db, "db", "d", "", "Enter MySQL database")
 	genCmd.Flags().StringVarP(&table, "table", "t", "", "Enter MySQL table")
-	genCmd.Flags().StringVarP(&serverName, "serverName", "s", "", "Enter project server name")
+	genCmd.Flags().StringVarP(&goVersion, "key", "", "id", "Enter table primary key")
+	genCmd.Flags().StringVarP(&serverName, "server_name", "s", "", "Enter project server name")
+	genCmd.Flags().StringVarP(&goModule, "go_module", "", "tmp", "Enter project go module name")
+	genCmd.Flags().StringVarP(&codePath, "code_path", "", "./tmp", "Enter project code generation path")
+	genCmd.Flags().StringVarP(&goVersion, "go_version", "", "1.20", "Enter project go version")
+	if len(viper.AllKeys()) == 0 {
+		viper.BindPFlag("mysql.addr", genCmd.Flags().Lookup("addr"))
+		viper.BindPFlag("mysql.user", genCmd.Flags().Lookup("user"))
+		viper.BindPFlag("mysql.pwd", genCmd.Flags().Lookup("pwd"))
+		viper.BindPFlag("mysql.port", genCmd.Flags().Lookup("port"))
+		viper.BindPFlag("mysql.db", genCmd.Flags().Lookup("db"))
+		viper.BindPFlag("mysql.table", genCmd.Flags().Lookup("table"))
+		viper.BindPFlag("mysql.key", genCmd.Flags().Lookup("key"))
+		viper.BindPFlag("server.server_name", genCmd.Flags().Lookup("server_name"))
+		viper.BindPFlag("server.go_module", genCmd.Flags().Lookup("go_module"))
+		viper.BindPFlag("server.code_path", genCmd.Flags().Lookup("code_path"))
+		viper.BindPFlag("server.go_version", genCmd.Flags().Lookup("go_version"))
+	}
 }
 
 func help() {
